12ff0c509ec5dd00c1e87d1db260969de2b093a4
Umair Habib <umairhabibx123@gmail.com>
2024-12-28 17:38:29 +0500

    Initial Commit8

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,13 @@
-export const webpack = {
-  configure: {
-    resolve: {
-      fallback: {
-        path: require.resolve('path-browserify'),
-        os: require.resolve('os-browserify/browser'),
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
+module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          path: require.resolve('path-browserify'),
+          os: require.resolve('os-browserify/browser'),
+          crypto: require.resolve('crypto-browserify'),
+          stream: require.resolve('stream-browserify'),
+        },
       },
     },
   },
